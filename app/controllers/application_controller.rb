2a0b0125b05a1bdbbf802c334ecf0c560c1f3f03
AyanaZaire <ayanazaire@gmail.com>
2019-08-16 14:58:50 -0400

    enabled sessions + built user show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    #i need to enable sessions
+    enable :sessions
+    #i need to set session something
+    set :session_secret, "our_super_secret_session_secret"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,24 @@ class UsersController < ApplicationController
 
   #create a session
   post '/login' do
-    #recieve the input from login form
-    #find the user
+    #[x] recieve the input from login form
+    #[x] find the user
+    @user = User.find_by(email: params[:email])
     #authenticate the user
-    #log them in - creating a session
-    binding.pry
+    if @user.authenticate(params[:password])
+      #[x] log them in - creating a session, adding a key/value pair to session hash
+      session[:user_id] = @user.id
+      #[x] redirects to user profile
+      redirect "users/#{@user.id}"
+    else
+      #show an error
+      #redirect to login form
+    end
   end
 
+  #user SHOW route
+  get '/users/:id' do
+    "user profile page!"
+  end
 
 end
