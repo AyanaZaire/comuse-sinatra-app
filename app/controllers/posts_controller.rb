06977412d8ed29ab36f4ae2463f0d30e391ce86a
AyanaZaire <ayanazaire@gmail.com>
2019-08-21 16:08:51 -0400

    added create funtionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,16 +9,25 @@ class PostsController < ApplicationController
     erb :'posts/index'
   end
 
+  # CREATE
+  # get 'post/new' render a form to create a new post
+  get '/posts/new' do
+    erb :'/posts/new'
+  end
+
+  # post route to create a new post
+  post "/posts" do
+    @post = Post.create(title: params[:title], image_url: params[:image_url], description: params[:description], user_id: current_user.id)
+
+    redirect "/posts/#{@post.id}"
+  end
+
   # show out for single post
   get '/posts/:id' do
     # id is coming from url - params
     @post = Post.find(params[:id])
     erb :'posts/show'
   end
-
-  # CREATE
-  # get 'post/new' render a form to create a new post
-  # post route to create a new post
 
   # UPDATE
   # get 'post/edit' route to render the form for editing a post
